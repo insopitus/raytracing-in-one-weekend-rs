f0cb40eda5cd9dede57ce3cb05541082aa6f0bcf
insopitus <jessefor@hotmail.com>
2024-03-10 22:50:16 +0800

    you can now change camera position and direction

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,18 @@ use renderer::{Material, MaterialKind, Renderer, Scene};
 use crate::renderer::Camera;
 mod renderer;
 
-const WIDTH: u32 = 1280;
-const HEIGHT: u32 = 720;
+
 
 fn main() {
     // let bar = indicatif::ProgressBar::new((WIDTH * HEIGHT) as u64);
-    let camera = Camera::new(1280, 760);
+    let mut camera = Camera::new(1280, 760);
+    camera.move_to(vec3(0.8,0.4,0.4));
+    camera.look_at(vec3(0.0,0.0,-0.5));
     // let sphere1 = Sphere::new(vec3(0.0, 0.0, -1.0), 0.5);
     // let sphere1 = Sphere::new(vec3(0.0, -100.5, -1.0), 100.0);
     // let mut geometries = vec![sphere];
     let mut scene = Scene::new();
-
+    // return;
     // glass ball
     // scene.add(
     //     Sphere::new(vec3(-0.3, -0.2, -0.8), 0.2),
@@ -39,7 +40,7 @@ fn main() {
     // );
 
     scene.add(
-        Sphere::new(vec3(-1.0, 0.0, -1.0), 0.5),
+        Sphere::new(vec3(0.0, 0.0, 0.0), 0.5),
         Material {
             kind: MaterialKind::Metal { fuzz: 0.2 },
             color: rgba(0.7, 0.3, 0.3, 1.0),
@@ -56,11 +57,11 @@ fn main() {
 
     scene.add(
         AxisAlignedBox::new(
-            vec3(0.4, -0.4, -1.1),
-            vec3(0.8, -0.1, -0.8),
+            vec3(0.4, -0.2, -0.8),
+            vec3(0.8, 0.2, -0.4),
         ),
         Material {
-            kind: MaterialKind::Lambertian,
+            kind: MaterialKind::Dielectric { fraction_rate: 1.5 },
             color: rgba(1.0, 1.0, 1.0, 1.0),
         },
     );
--- src/renderer.rs
+++ src/renderer.rs
@@ -10,7 +10,7 @@ use lib_rs::{
     color::{self, mix, rgba, Color},
     geometry::Sphere,
     linear_algebra::{
-        vector::{dot, vec3},
+        vector::{cross, dot, vec3},
         Vector3,
     },
     ray::{HitRecord, Hitable, Ray},
@@ -49,11 +49,13 @@ pub fn random_vec3_on_semisphere(rng: &mut rand::rngs::ThreadRng, normal: Vector
     }
 }
 
+const VIEWPORT_HEIGHT_3D: f32 = 2.0;
+
 #[derive(Debug)]
 pub struct Camera {
     aspect_ratio: f32,
-    pub frame_size: (u32, u32),
-    center: Vector3,
+    frame_size: (u32, u32),
+    position: Vector3,
     pixel_delta_u: Vector3,
     pixel_delta_v: Vector3,
     pixel_0_loc: Vector3,
@@ -64,24 +66,50 @@ impl Camera {
     pub fn new(width: u32, height: u32) -> Self {
         let aspect_ratio = width as f32 / height as f32;
         let center = Vector3::ZERO;
+        let view_dir = vec3(0.0, 0.0, -1.0);
+
         // near plane
-        let viewport_size = (2.0 * aspect_ratio, 2.0);
-        let pixel_size = 2.0 / height as f32;
-        let viewport_top_left = center
-            + vec3(0.0, 0.0, -1.0)
-            + vec3(-viewport_size.0 / 2.0, viewport_size.1 / 2.0, 0.0);
-        let pixel_delta_u = vec3(pixel_size, 0.0, 0.0);
-        let pixel_delta_v = vec3(0.0, -pixel_size, 0.0);
-        let pixel_0_loc = viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        let (pixel_0_loc, pixel_delta_u, pixel_delta_v, viewport_size) =
+            Self::calc_near_plane_values(center, view_dir, aspect_ratio, (width, height));
         Self {
             aspect_ratio,
             frame_size: (width, height),
-            center,
+            position: center,
             pixel_0_loc,
             pixel_delta_u,
             pixel_delta_v,
             viewport_size,
         }
+    }
+    fn calc_near_plane_values(
+        center: Vector3,
+        view_dir: Vector3,
+        aspect_ratio: f32,
+        frame_size: (u32, u32),
+    ) -> (Vector3, Vector3, Vector3, (f32, f32)) {
+        let dir = view_dir;
+        let left = cross(vec3(0.0, 1.0, 0.0), dir).normalize();
+        // up direction of the frame in 3d space;
+        let up = cross(dir, left);
+        let viewport_size = (VIEWPORT_HEIGHT_3D * aspect_ratio, VIEWPORT_HEIGHT_3D);
+        let pixel_size = 2.0 / (frame_size.1 as f32);
+        let viewport_top_left = center + dir + viewport_size.0/2.0 * left + viewport_size.1/2.0 * up;
+        let pixel_delta_u = pixel_size * -left;
+        let pixel_delta_v = pixel_size * -up;
+        let pixel_0_loc = viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        (pixel_0_loc, pixel_delta_u, pixel_delta_v, viewport_size)
+    }
+    pub fn move_to(&mut self, pos: Vector3) {
+        self.pixel_0_loc = self.pixel_0_loc + (pos - self.position);
+        self.position = pos;
+    }
+    pub fn look_at(&mut self, target: Vector3) {
+        let dir = (target - self.position).normalize();
+        let (loc_0,delta_u,delta_v,_) = Self::calc_near_plane_values(self.position, dir, self.aspect_ratio, self.frame_size);
+        self.pixel_0_loc = loc_0;
+        self.pixel_delta_u = delta_u;
+        self.pixel_delta_v = delta_v;
+        // left direction of the frame in 3d space;
     }
     pub fn get_ray_at(&self, u: u32, v: u32, rng: &mut rand::rngs::ThreadRng) -> Ray {
         // let dir =
@@ -94,12 +122,15 @@ impl Camera {
         let dir = self.pixel_0_loc
             + (u as f32 + rand_x) * self.pixel_delta_u
             + (v as f32 + rand_y) * self.pixel_delta_v
-            - self.center;
+            - self.position;
 
         Ray {
-            origin: self.center,
+            origin: self.position,
             direction: dir.normalize(),
         }
+    }
+    pub fn frame_size(&self) -> (u32, u32) {
+        self.frame_size
     }
 }
 
